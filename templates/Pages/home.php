c33630a1d270b373d90a1a4d0a4adcc6aa80dc18
Junio28 <ejuniormartinez@uniguajira.edu.co>
2021-12-07 17:39:55 -0500

    Complementar con el menú y el estilo Bootstrap

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -53,17 +53,64 @@ $cakeDescription = 'CakePHP: the rapid development PHP framework';
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-     <link rel="stylesheet" href="css/cake.css">
-    <link rel="stylesheet" href="css/home.css">
-    <link rel="stylesheet" href="css/milligram.min.css">
-    <link rel="stylesheet" href="css/normalize.min.css">
+    <title>
+        <?= $cakeDescription ?>: Pagina Principal
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <?= $this->Html->css(['bootstrap.min']) ?>
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?> 
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="visually-hidden">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="d-flex"">
+      <input class="form-control me-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    
     <div class="container">
-        <h2>Bienvenidos a CakePHP 4.3.2</h2>
+    <h2>Bienvenidos a CakePHP <?= Configure::version() ?> </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit consequatur fugit, incidunt corrupti illo nam nulla dolorem assumenda voluptatum, explicabo ipsa reprehenderit vel? Optio voluptas dicta beatae quia fuga ad!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse sapiente dolor unde eligendi in! Odit dicta commodi harum? Unde voluptatibus commodi cum iure quibusdam? Beatae velit consequuntur nulla quo cupiditate.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae maxime non impedit, ut voluptatum iusto placeat hic, voluptatem magni vel autem ullam quidem quis. Perferendis deleniti error consequuntur provident maxime!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil voluptas accusamus eaque fugit quas ut dolore aut modi unde ipsa voluptatibus numquam corporis debitis esse excepturi, quod soluta, odit cum?</p>
     </div>
 </body>
 </html>
